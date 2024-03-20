c9f911906b55fe3293bcb207b4ac48c2871fb672
logesh@ncash.ai <logesh.sightspectrum@gmail.com>
2024-03-20 07:25:44 +0530

    ui changed a bit

--- CoreMlLearning/ContentView.swift
+++ CoreMlLearning/ContentView.swift
@@ -53,6 +53,7 @@ struct ContentView: View {
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+//    @Statet private var 
     
    static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -61,28 +62,56 @@ struct ContentView: View {
         return Calendar.current.date(from: components) ?? .now
     }
     
-    func calculateBetTime(){
-        
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: expectingSleepHrs, coffee: Int64(Double(cupOfCoffeeIntake)))
-            
-            let sleepTime = wakeUp - prediction.actualSleep
-
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-        } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+//    func calculateBetTime(){
+//        
+//        do {
+//            let config = MLModelConfiguration()
+//            let model = try SleepCalculator(configuration: config)
+//            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+//            let hour = (components.hour ?? 0) * 60 * 60
+//            let minute = (components.minute ?? 0) * 60
+//            
+//            let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: expectingSleepHrs, coffee: Int64(Double(cupOfCoffeeIntake)))
+//            
+//            let sleepTime = wakeUp - prediction.actualSleep
+//
+//            alertTitle = "Your ideal bedtime is…"
+//            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+//        } catch {
+//            alertTitle = "Error"
+//            alertMessage = "Sorry, there was a problem calculating your bedtime."
+//        }
+//        
+//        showingAlert = true
+//        
+//    }
+    
+        func calculateBetTime()->String{
+    
+            do {
+                let config = MLModelConfiguration()
+                let model = try SleepCalculator(configuration: config)
+                let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+                let hour = (components.hour ?? 0) * 60 * 60
+                let minute = (components.minute ?? 0) * 60
+    
+                let prediction = try model.prediction(wake: Int64(Double(hour + minute)), estimatedSleep: expectingSleepHrs, coffee: Int64(Double(cupOfCoffeeIntake)))
+    
+                let sleepTime = wakeUp - prediction.actualSleep
+    
+//                alertTitle = "Your ideal bedtime is…"
+                return sleepTime.formatted(date: .omitted, time: .shortened)
+            } catch {
+//                alertTitle = "Error"
+                return "Sorry, there was a problem calculating your bedtime."
+            }
+    
+//            showingAlert = true
+    
         }
-        
-        showingAlert = true
-        
+    
+    var result: String {
+        calculateBetTime()
     }
     
     
@@ -90,24 +119,32 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("when do you want to wake up").font(.headline)
+                Section("when do you want to wake up") {
+//                    Text("when do you want to wake up").font(.headline)
                 
                 
                 DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute).labelsHidden()
                 }
+          
             
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
+                Section("Daily coffee intake") {
+//                    Text("Daily coffee intake")
+//                        .font(.headline)
                     
-                    Stepper("^[\(cupOfCoffeeIntake) cup](inflect: true)", value: $cupOfCoffeeIntake, in:0...20)
+//                    Stepper("^[\(cupOfCoffeeIntake) cup](inflect: true)", value: $cupOfCoffeeIntake, in:0...20)
+                    
+                    Picker("^[\(cupOfCoffeeIntake) cup](inflect: true)", selection: $cupOfCoffeeIntake){
+                        ForEach(0...20, id: \.self) { i in
+                                Text("\(i)").bold()
+                            }
+//                        ForEach(0...20, id: , content: <#T##(Binding<C.Element>) -> _#>)
+                    }
                     
                 }
             
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
+                Section("Desired amount of sleep") {
+//                    Text("Desired amount of sleep")
+//                        .font(.headline)
 
                     
                         Stepper("\(expectingSleepHrs.formatted()) expecting sleep hrs", value:$expectingSleepHrs, in:4...12, step:0.25).alert(alertTitle, isPresented: $showingAlert){
@@ -116,9 +153,18 @@ struct ContentView: View {
                             Text(alertMessage)
                         }
                 }
-            }.navigationTitle("Better Rest").toolbar{
-                Button("calculate", action: calculateBetTime)
-        }
+                
+                Section("Time For Bed"){
+                    VStack(alignment: .center, spacing: 10 ){
+                        Text("\(result)").font(.title)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.white)
+                                        .padding()
+                                        .background(Color.blue)
+                                        .cornerRadius(10)
+                    }.frame(maxWidth: .infinity)
+                }
+            }.navigationTitle("Better Rest")
         }
     }
     
